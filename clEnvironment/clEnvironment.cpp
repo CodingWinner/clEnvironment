74f7fab3082ccbfab2dc612a9c865aa2ad020dc6
CodingWinner <hellosapna1982@gmail.com>
2025-01-22 17:40:46 -0500

    Add project files.

---
+++ clEnvironment/clEnvironment.cpp
@@ -0,0 +1,12 @@
+﻿// clEnvironment.cpp : Defines the entry point for the application.
+//
+
+#include "clEnvironment.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
